b2acce12c7f798bc56e0175c0d4bc2bb303753b8
kuldeep354 <120399710+kuldeep354@users.noreply.github.com>
2022-12-12 11:03:28 -0500

    link updated
    link updated

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Kuldeep Singh
+*  Student ID: 127966216
+*  Date: 12/12/2022 
+* 
+*  Online (Cyclic) URL: https://vast-plum-narwhal-veil.cyclic.app/ 
+* 
+********************************************************************************/  
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
